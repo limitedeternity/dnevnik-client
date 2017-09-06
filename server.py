6c1007bbb1a00ba2ab2965d88374dbd725ca6942
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-09-06 23:14:47 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -169,7 +169,12 @@ def index():
                         return response
 
                 else:
-                    return dumps("Вход уже выполнен ¯\_(ツ)_/¯", ensure_ascii=False)
+                    response = make_response(redirect('/'))
+                    response.headers['X-Content-Type-Options'] = 'nosniff'
+                    response.headers['X-Frame-Options'] = 'DENY'
+                    response.headers['X-XSS-Protection'] = '1; mode=block'
+                    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+                    return response
 
         else:
             return True
