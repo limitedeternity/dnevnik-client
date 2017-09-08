52de24cf5f63d20f726f48c0100f39d3015a0df7
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-09-08 23:33:30 +0300

    Add files via upload

--- static/js/dnevnik.operator.js
+++ static/js/dnevnik.operator.js
@@ -27,6 +27,9 @@ $(document).ready(function() {
 
     $("#dnevnik-date").on("submit", function(a) {
         a.preventDefault();
+
+        $("#dnevnik-out").html("<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4><div class='section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone'><div class='section__circle-container__circle mdl-color--primary'></div></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><h5>Подождите...</h5><div class='loader'>Loading...</div></div>");
+
         $.ajax({
                 headers: {
                     "X-CSRFToken": Cookies.get("csrftoken")
