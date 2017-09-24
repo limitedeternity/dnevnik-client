9bbd695f7ba465eb97009731dd4eafb0ab9afdf9
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-09-24 18:11:45 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1112,7 +1112,13 @@ def log_in():
 
 @app.route("/apply", methods=['POST'])
 def apply():
-    response = make_response(jsonify("Успешно."))
+
+    html_out = ""
+
+    html_out += '<div style="display:block; height:2px; clear:both;"></div>'
+    html_out += '<p style="text-align:center; color:green;">Смена цветовой схемы успешна ^^</p>'
+
+    response = make_response(jsonify(html_out))
 
     color = request.form.get('color', '')
     response.set_cookie('Theme', value=color, max_age=2592000, expires=2592000)
