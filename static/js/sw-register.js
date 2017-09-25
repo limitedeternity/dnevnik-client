91b78a7600a6d326c9a7639a898fcfd7e3bec800
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-09-25 17:41:28 +0300

    Update sw-register.js

--- static/js/sw-register.js
+++ static/js/sw-register.js
@@ -1,15 +1,1 @@
-if ('serviceWorker' in navigator) {
-  if (navigator.serviceWorker.controller) {
-      console.log('Active ServiceWorker found, no need to register');
-  } else {
-      navigator.serviceWorker.register('/sw.js', {
-        scope: './'
-      }).then(function() {
-          console.log('ServiceWorker registration complete.');
-      }, function() {
-          console.log('ServiceWorker registration failure.');
-      });
-    }
-} else { 
-    console.log('ServiceWorker is not supported.');
-}
+'serviceWorker'in navigator?navigator.serviceWorker.controller?console.log('Active ServiceWorker found, no need to register'):navigator.serviceWorker.register('/sw.js',{scope:'./'}).then(function(){console.log('ServiceWorker registration complete.')},function(){console.log('ServiceWorker registration failure.')}):console.log('ServiceWorker is not supported.');
