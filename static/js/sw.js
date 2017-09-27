f9fc74e0f1dcf89066f89532e5d871e30754b903
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-09-27 22:03:01 +0300

    Update sw.js

--- static/js/sw.js
+++ static/js/sw.js
@@ -84,3 +84,19 @@ var returnFromCache = function(request){
     });
   });
 };
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
