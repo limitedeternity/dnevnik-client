f67d83529a88b3411d9b54f7de162ea9ec8d0261
Marise Hayashi <vyatheslav2707@mail.ru>
2018-01-17 00:15:04 +0300

    Performance optimization

--- static/js/uncompressed/dnevnik.operator.js
+++ static/js/uncompressed/dnevnik.operator.js
@@ -7,8 +7,8 @@
   if (location.pathname == "/") {
       if (Cookies.get('AccessToken') !== undefined) {
         whenDomReady().then(() => {
-          document.querySelector("#nav").innerHTML = '<a href="#overview" class="mdl-layout__tab is-active">Загрузка...</a>';
-          document.querySelector("#text").innerHTML = "<div class='loader'>Loading...</div>";
+          document.getElementById("nav").innerHTML = '<a href="#overview" class="mdl-layout__tab is-active">Загрузка...</a>';
+          document.getElementById("text").innerHTML = "<div class='loader'>Loading...</div>";
           location.replace("/main");
         });
 
@@ -19,14 +19,14 @@
       } else {
 
         whenDomReady().then(() => {
-          document.querySelector("#button-login").addEventListener("click", (event) => {
+          document.getElementById("button-login").addEventListener("click", (event) => {
               event.preventDefault();
 
               if (navigator.onLine) {
                   location.href = "https://login.dnevnik.ru/oauth2?response_type=token&client_id=0925b3b0d1e84c05b85851e4f8a4033d&scope=CommonInfo,FriendsAndRelatives,EducationalInfo&redirect_uri=https://dnevnik-client.herokuapp.com/";
 
               } else {
-                  document.querySelector("#error").innerHTML = '<div style="display:block; height:2px; clear:both;"></div><p style="text-align:center; color:red;">Оффлайн ¯\_(ツ)_/¯</p>';
+                  document.getElementById("error").innerHTML = '<div style="display:block; height:2px; clear:both;"></div><p style="text-align:center; color:red;">Оффлайн ¯\_(ツ)_/¯</p>';
               }
           });
         });
@@ -57,14 +57,14 @@
           fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({".": "1"}), credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then((json) => {
-            localforage.setItem('dnevnik', json).then(() => {
-              whenDomReady().then(() => {
+              localforage.setItem('dnevnik', json).then(() => {
+                whenDomReady.resume();
+              }).then(() => {
                 localforage.getItem('dnevnik').then((data) => {
-                  document.querySelector("#dnevnik-out").innerHTML = data;
+                  document.getElementById("dnevnik-out").innerHTML = data;
                 });
               });
             });
-          });
         })
       );
 
@@ -72,14 +72,14 @@
         fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({".": "1"}), credentials: 'same-origin'}).then((response) => {
             return response.json();
           }).then((json) => {
-          localforage.setItem('stats', json).then(() => {
-            whenDomReady().then(() => {
+            localforage.setItem('stats', json).then(() => {
+              whenDomReady.resume();
+            }).then(() => {
               localforage.getItem('stats').then((data) => {
-                document.querySelector("#stats-out").innerHTML = data;
+                document.getElementById("stats-out").innerHTML = data;
               });
             });
-          });
-        })
+          })
       );
 
     } else {
@@ -87,7 +87,7 @@
         if (data === null) {
           promiseChain.push(
             whenDomReady().then(() => {
-              document.querySelector("#stats-out").innerHTML = '<h4 class="mdl-cell mdl-cell--12-col">Статистика</h4><div class="section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone"><i class="material-icons mdl-list__item-avatar mdl-color--primary" style="font-size:32px; padding-top:2.5px; text-align:center;"></i></div><div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone"><h5>Данные не получены ¯\_(ツ)_/¯</h5>Кажется, Вы в оффлайне :> </div>';
+              document.getElementById("stats-out").innerHTML = '<h4 class="mdl-cell mdl-cell--12-col">Статистика</h4><div class="section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone"><i class="material-icons mdl-list__item-avatar mdl-color--primary" style="font-size:32px; padding-top:2.5px; text-align:center;"></i></div><div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone"><h5>Данные не получены ¯\_(ツ)_/¯</h5>Кажется, Вы в оффлайне :> </div>';
             })
           );
 
@@ -95,7 +95,7 @@
           promiseChain.push(
             whenDomReady().then(() => {
               localforage.getItem('stats').then((data) => {
-                document.querySelector("#stats-out").innerHTML = data;
+                document.getElementById("stats-out").innerHTML = data;
               });
             })
           );
@@ -107,7 +107,7 @@
         if (data === null) {
           promiseChain.push(
             whenDomReady().then(() => {
-              document.querySelector("#dnevnik-out").innerHTML = '<h4 class="mdl-cell mdl-cell--12-col">Дневник</h4><div class="section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone"><i class="material-icons mdl-list__item-avatar mdl-color--primary" style="font-size:32px; padding-top:2.5px; text-align:center;"></i></div><div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone"><h5>Данные не получены ¯\_(ツ)_/¯</h5>Кажется, Вы в оффлайне :> </div>';
+              document.getElementById("dnevnik-out").innerHTML = '<h4 class="mdl-cell mdl-cell--12-col">Дневник</h4><div class="section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone"><i class="material-icons mdl-list__item-avatar mdl-color--primary" style="font-size:32px; padding-top:2.5px; text-align:center;"></i></div><div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone"><h5>Данные не получены ¯\_(ツ)_/¯</h5>Кажется, Вы в оффлайне :> </div>';
             })
           );
 
@@ -115,7 +115,7 @@
           promiseChain.push(
             whenDomReady().then(() => {
               localforage.getItem('dnevnik').then((data) => {
-                document.querySelector("#dnevnik-out").innerHTML = data;
+                document.getElementById("dnevnik-out").innerHTML = data;
               });
             })
           );
@@ -126,46 +126,37 @@
     Promise.all(promiseChain).catch((error) => {console.log(error)});
 
     whenDomReady().then(() => {
-      document.querySelector("#dnevnik-date").addEventListener("submit", (event) => {
+      document.getElementById("dnevnik-date").addEventListener("submit", (event) => {
           event.preventDefault();
           let form = event.target;
 
           if (!navigator.onLine) {return;}
-          document.querySelector("#dnevnik-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
+          document.getElementById("dnevnik-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
 
           new Promise((resolve) => (Cookies.set("Offset", -new Date().getTimezoneOffset() / 60) && resolve())).then(() => {
             fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
                 return response.json();
               }).then((json) => {
-              localforage.setItem('dnevnik', json).then(() => {
-                localforage.getItem('dnevnik').then((data) => {
-                    document.querySelector("#dnevnik-out").innerHTML = data;
-                    setTimeout(() => {form.submit()}, 1000 * 60 * 4);
-                });
+                document.getElementById("dnevnik-out").innerHTML = data;
               });
-            });
           });
       });
 
-      document.querySelector("#dnevnik-stats").addEventListener("submit", (event) => {
+      document.getElementById("dnevnik-stats").addEventListener("submit", (event) => {
           event.preventDefault();
           let form = event.target;
 
           if (!navigator.onLine) {return;}
-          document.querySelector("#stats-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Статистика</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
+          document.getElementById("stats-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Статистика</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
 
           fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then((json) => {
-            localforage.setItem('stats', json).then(() => {
-              localforage.getItem('stats').then((data) => {
-                document.querySelector("#stats-out").innerHTML = data;
-              });
+              document.getElementById("stats-out").innerHTML = data;
             });
-          });
       });
 
-      document.querySelector("#dnevnik-settings").addEventListener("submit", (event) => {
+      document.getElementById("dnevnik-settings").addEventListener("submit", (event) => {
           event.preventDefault();
           let form = event.target;
 
@@ -173,11 +164,11 @@
           fetch("/apply", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then(async (json) => {
-            document.querySelector("#error").innerHTML = json;
+            document.getElementById("error").innerHTML = json;
 
             if (json.includes("color:red;")) {
               await sleep(3000);
-              document.querySelector("#error").innerHTML = '';
+              document.getElementById("error").innerHTML = '';
               return;
             }
 
@@ -185,7 +176,7 @@
           });
      });
 
-     document.querySelector("#logout").addEventListener("click", (event) => {
+     document.getElementById("logout").addEventListener("click", (event) => {
          event.preventDefault();
 
          if (navigator.onLine) {
@@ -194,7 +185,7 @@
          }
      });
 
-     document.querySelector("#reset-storage").addEventListener("click", (event) => {
+     document.getElementById("reset-storage").addEventListener("click", (event) => {
          event.preventDefault();
 
          if (navigator.onLine) {
@@ -203,7 +194,7 @@
          }
      });
 
-     document.querySelector("#reset-sw").addEventListener("click", (event) => {
+     document.getElementById("reset-sw").addEventListener("click", (event) => {
          event.preventDefault();
 
          if (navigator.onLine) {
