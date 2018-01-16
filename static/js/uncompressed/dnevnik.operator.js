363385ae0e6371075ea02afec3b8d8380aa17416
Marise Hayashi <vyatheslav2707@mail.ru>
2018-01-16 20:20:44 +0300

    Made him to send cookies with Fetch

--- static/js/uncompressed/dnevnik.operator.js
+++ static/js/uncompressed/dnevnik.operator.js
@@ -33,7 +33,7 @@
 
       promiseChain.push(
         new Promise((resolve) => (Cookies.set("Offset", -new Date().getTimezoneOffset() / 60) && resolve())).then(() => {
-          fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}}).then((response) => {
+          fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}, credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then((json) => {
             localforage.setItem('dnevnik', json).then(() => {
@@ -48,7 +48,7 @@
       );
 
       promiseChain.push(
-        fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}}).then((response) => {
+        fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}, credentials: 'same-origin'}).then((response) => {
             return response.json();
           }).then((json) => {
           localforage.setItem('stats', json).then(() => {
@@ -113,7 +113,7 @@
           document.querySelector("#dnevnik-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
 
           new Promise((resolve) => (Cookies.set("Offset", -new Date().getTimezoneOffset() / 60) && resolve())).then(() => {
-            fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form)}).then((response) => {
+            fetch("/dnevnik", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
                 return response.json();
               }).then((json) => {
               localforage.setItem('dnevnik', json).then(() => {
@@ -133,7 +133,7 @@
           if (!navigator.onLine) {return;}
           document.querySelector("#stats-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Статистика</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
 
-          fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form)}).then((response) => {
+          fetch("/stats", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then((json) => {
             localforage.setItem('stats', json).then(() => {
@@ -149,7 +149,7 @@
           let form = event.target;
 
           if (!navigator.onLine) {return;}
-          fetch("/apply", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form)}).then((response) => {
+          fetch("/apply", {method: 'POST', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
               return response.json();
             }).then((json) => {
             document.querySelector("#error").innerHTML(json);
