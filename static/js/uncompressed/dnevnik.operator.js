b0501264c91b0a65039b8c53f40ab96eb155c7cd
Marise Hayashi <vyatheslav2707@mail.ru>
2018-02-03 18:06:30 +0300

    Fixes

--- static/js/uncompressed/dnevnik.operator.js
+++ static/js/uncompressed/dnevnik.operator.js
@@ -151,77 +151,80 @@
     );
 
     whenDomReady().then(() => {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.sync.getTags().then(async (tags) => {
-            while (!!tags.length) {
+      var not_checked = true;
+      while (not_checked) {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.sync.getTags().then(async (tags) => {
+              if (tags.length === 0) {
+                not_checked = false;
+              }
               await sleep(10);
-            }
-
-            return Promise.all(promiseChain).then(() => {
-              document.getElementById("dnevnik-date").addEventListener("submit", (event) => {
-                  event.preventDefault();
-                  let form = event.target;
-
-                  if (!navigator.onLine) {
-                    return;
-                  }
-
-                  document.getElementById("dnevnik-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
-
-                  fetch("/dnevnik", {method: 'POST', redirect: 'follow', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((responseDnevnik) => {
-                        return response.json();
-                      }).then((jsonDnevnik) => {
-                        document.getElementById("dnevnik-out").innerHTML = jsonDnevnik;
-                      })
-              });
-
-              document.getElementById("dnevnik-settings").addEventListener("submit", (event) => {
-                  event.preventDefault();
-                  let form = event.target;
-
-                  if (!navigator.onLine) {return;}
-                  fetch("/apply", {method: 'POST', redirect: 'follow', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
-                      return response.json();
-                    }).then(async (json) => {
-                    document.getElementById("error").innerHTML = json;
-
-                    if (json.includes("color:red;")) {
-                      await sleep(3000);
-                      document.getElementById("error").innerHTML = '';
-                      return;
-                    }
-
-                    await sleep(500);
-                    location.replace("/");
-                  });
-             });
-
-             document.getElementById("logout").addEventListener("click", (event) => {
-                 event.preventDefault();
-                 if (navigator.onLine) {
-                     localforage.clear();
-                     if ('serviceWorker' in navigator) {
-                       navigator.serviceWorker.getRegistrations().then((t) => {t.forEach((k) => {k.unregister()})});
-                     }
-                     location.replace("/logout");
-                 }
-             });
-
-             document.getElementById("reset-storage").addEventListener("click", (event) => {
-                 event.preventDefault();
-                 if (navigator.onLine) {
-                     localforage.clear();
-                     location.reload();
-                 }
-             });
-
-             if (Notification.permission !== 'denied' || Notification.permission === "default") {
-               Notification.requestPermission();
-             }
-            })
+          })
         })
+      }
+
+      return Promise.all(promiseChain).then(() => {
+        document.getElementById("dnevnik-date").addEventListener("submit", (event) => {
+            event.preventDefault();
+            let form = event.target;
+
+            if (!navigator.onLine) {return;}
+
+            document.getElementById("dnevnik-out").innerHTML = "<h4 class='mdl-cell mdl-cell--12-col'>Дневник</h4></div><div class='section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone'><div class='loader'>Loading...</div></div>";
+
+            fetch("/dnevnik", {method: 'POST', redirect: 'follow', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((responseDnevnik) => {
+                  return response.json();
+                }).then((jsonDnevnik) => {
+                  document.getElementById("dnevnik-out").innerHTML = jsonDnevnik;
+                })
+        });
+
+        document.getElementById("dnevnik-settings").addEventListener("submit", (event) => {
+            event.preventDefault();
+            let form = event.target;
+
+            if (!navigator.onLine) {return;}
+
+            fetch("/apply", {method: 'POST', redirect: 'follow', headers: {'Content-Type': 'application/json'}, body: serialize(form), credentials: 'same-origin'}).then((response) => {
+                return response.json();
+              }).then(async (json) => {
+              document.getElementById("error").innerHTML = json;
+
+              if (json.includes("color:red;")) {
+                await sleep(3000);
+                document.getElementById("error").innerHTML = '';
+                return;
+              }
+
+              await sleep(500);
+              location.replace("/");
+            });
+       });
+
+       document.getElementById("logout").addEventListener("click", (event) => {
+           event.preventDefault();
+           if (navigator.onLine) {
+               localforage.clear();
+               if ('serviceWorker' in navigator) {
+                 navigator.serviceWorker.getRegistrations().then((t) => {t.forEach((k) => {k.unregister()})});
+               }
+               location.replace("/logout");
+           }
+       });
+
+       document.getElementById("reset-storage").addEventListener("click", (event) => {
+           event.preventDefault();
+           if (navigator.onLine) {
+               localforage.clear();
+               location.reload();
+           }
+       });
+
+       if (Notification.permission !== 'denied' || Notification.permission === "default") {
+         Notification.requestPermission();
+       }
       })
-    });
+    })
   }
 
   HTMLDocument.prototype.__defineGetter__("write", () => {
