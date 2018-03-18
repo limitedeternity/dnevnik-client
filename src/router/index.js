10fba88cd2e15501d0d54e139b2c3d76fe7d4758
Marise Hayashi <vyatheslav2707@mail.ru>
2018-03-18 12:18:06 +0300

    Navscroll

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,56 @@ const router = new Router({
 });
 
 router.afterEach(() => {
-    window.scrollTo(0, 0);
+    window.requestAnimFrame = (() => {
+        return window.requestAnimationFrame || 
+               window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame;
+    })();
+
+    var scrollToY = (scrollTargetY, speed, easing) => {
+
+        var scrollY = window.scrollY || document.documentElement.scrollTop,
+            scrollTargetY = scrollTargetY || 0,
+            speed = speed || 2000,
+            easing = easing || 'easeOutSine',
+            currentTime = 0;
+
+        var time = Math.max(0.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8));
+
+        var easingEquations = {
+            easeOutSine: (pos) => {
+                return Math.sin(pos * (Math.PI / 2));
+            },
+            easeInOutSine: (pos) => {
+                return (-0.5 * (Math.cos(Math.PI * pos) - 1));
+            },
+            easeInOutQuint: (pos) => {
+                if ((pos /= 0.5) < 1) {
+                    return 0.5 * Math.pow(pos, 5);
+                }
+                return 0.5 * (Math.pow((pos - 2), 5) + 2);
+            }
+        };
+
+        var tick = () => {
+            currentTime += 1 / 60;
+
+            var p = currentTime / time;
+            var t = easingEquations[easing](p);
+
+            if (p < 1) {
+                window.requestAnimFrame(tick);
+                window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+
+            } else {
+                window.scrollTo(0, scrollTargetY);
+            }
+        };
+
+        tick();
+    };
+
+    scrollToY(0, 400, 'easeInOutQuint');
 });
 
 export default router;
