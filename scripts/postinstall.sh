ca75d32258fbebef8f8fa8d246da8622b82ca49e
Marise Hayashi <vyatheslav2707@mail.ru>
2018-03-19 19:15:00 +0300

    Deps cleanup

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ $NODE_ENV == production ]]; then 
-    cross-env NODE_ENV=production webpack --progress --hide-modules
-    workbox generateSW workbox-config.prod.js
+    cross-env NODE_ENV=production webpack --progress --hide-modules;
+    workbox generateSW workbox-config.prod.js;
 fi;
