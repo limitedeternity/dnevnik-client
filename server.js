51a55a1c01454a6fce904db27dc21152f99420fb
Marise Hayashi <vyatheslav2707@mail.ru>
2018-07-06 12:17:26 +0300

    .

--- server.js
+++ server.js
@@ -5,43 +5,40 @@ const path = require('path');
 const fs = require('fs');
 
 
-setTimeout(() => {
-    var instance = express();
+var instance = express();
 
-    if (process.env.NODE_ENV === 'production') {
-        var PORT = '/tmp/nginx.socket';
-        var callbackFn = () => {
-            fs.closeSync(fs.openSync('/tmp/app-initialized', 'w'));
-            console.log(`Listening on ${PORT}`);
-        };
+if (process.env.NODE_ENV === 'production') {
+    var PORT = '/tmp/nginx.socket';
+    var callbackFn = () => {
+        fs.closeSync(fs.openSync('/tmp/app-initialized', 'w'));
+        console.log(`Listening on ${PORT}`);
+    };
 
-        instance.enable("trust proxy");
+    instance.enable("trust proxy");
 
-    } else {
-        var PORT = 8080;
-        var callbackFn = () => {
-            console.log(`Listening on ${PORT}`);
-        };
-    }
+} else {
+    var PORT = 8080;
+    var callbackFn = () => {
+        console.log(`Listening on ${PORT}`);
+    };
+}
 
-    instance.use(helmet());
-    instance.use(compression());
-    instance.use('/src/assets', express.static(path.join(__dirname, 'src', 'assets')));
-    instance.use('/dist', express.static(path.join(__dirname, 'dist')));
+instance.use(helmet());
+instance.use(compression());
+instance.use('/src/assets', express.static(path.join(__dirname, 'src', 'assets')));
+instance.use('/dist', express.static(path.join(__dirname, 'dist')));
 
-    instance.get('/sw.js', (req, res) => {
-        res.setHeader('Cache-Control', 'no-cache');
-        res.sendFile(path.join(__dirname, 'dist', 'sw.js'));
-    });
+instance.get('/sw.js', (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.sendFile(path.join(__dirname, 'dist', 'sw.js'));
+});
 
-    instance.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
+instance.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-    instance.get('*', (req, res) => {
-        res.redirect('/');
-    });
+instance.get('*', (req, res) => {
+    res.redirect('/');
+});
 
-    instance.listen(PORT, callbackFn);
-
-}, 2000);
+instance.listen(PORT, callbackFn);
